7562ad92ba2662f02a3d8d1a9771370761fb3f80
jht1493 <jht1493@gmail.com>
2021-06-14 18:29:06 -0400

    Update sketch.js

--- Day_1_50_Days_of_Video_Chat/sketch.js
+++ Day_1_50_Days_of_Video_Chat/sketch.js
@@ -7,30 +7,35 @@ var audioCtx;
 function setup() {
   createCanvas(1, 1);
   frameRate(2);
-  
+
   audioCtx = new AudioContext();
 
-  let constraints = {audio: true, "video": {
-        "width": 320,
-        "height": 240
-    }};
-  myVideo = createCapture(constraints, 
-    function(stream) {
-	  let p5l = new p5LiveMedia(this, "CAPTURE", stream, "Day 1 - 50 Days of Video Chat")
-	  p5l.on('stream', gotStream);
-      p5l.on('disconnect', gotDisconnect);
-    
-      var audioSource = audioCtx.createMediaStreamSource(stream); 
-      var analyser = audioCtx.createAnalyser();
-      var source = audioCtx.createMediaStreamSource(stream);
-      source.connect(analyser);
-      // Is there a callback I can use instead
-      myVideo.analyser = analyser;
-      otherVideos['me'] = myVideo;
+  let constraints = {
+    audio: true,
+    video: {
+      width: 320,
+      height: 240,
+    },
+  };
+  myVideo = createCapture(constraints, function (stream) {
+    let p5l = new p5LiveMedia(
+      this,
+      'CAPTURE',
+      stream,
+      'Day 1 - 50 Days of Video Chat'
+    );
+    p5l.on('stream', gotStream);
+    p5l.on('disconnect', gotDisconnect);
 
-    }
-  );  
-  myVideo.elt.muted = true;   
+    var audioSource = audioCtx.createMediaStreamSource(stream);
+    var analyser = audioCtx.createAnalyser();
+    var source = audioCtx.createMediaStreamSource(stream);
+    source.connect(analyser);
+    // Is there a callback I can use instead
+    myVideo.analyser = analyser;
+    otherVideos['me'] = myVideo;
+  });
+  myVideo.elt.muted = true;
 }
 
 function draw() {
@@ -40,24 +45,24 @@ function draw() {
     if (otherVideos.hasOwnProperty(v)) {
       // get the average, bincount is fftsize / 2
       //console.log(otherVideos[v]);
-      var array =  new Uint8Array(otherVideos[v].analyser.frequencyBinCount);
+      var array = new Uint8Array(otherVideos[v].analyser.frequencyBinCount);
       otherVideos[v].analyser.getByteFrequencyData(array);
-      var average = getAverageVolume(array)
-      //console.log('VOLUME:' + average); 
+      var average = getAverageVolume(array);
+      //console.log('VOLUME:' + average);
       if (average > loudness) {
         loudness = average;
-        loudest = v;        
+        loudest = v;
       }
     }
   }
   if (loudest != -1) {
-      for (const vl in otherVideos) {
-        if (loudest == vl) {
-          otherVideos[loudest].elt.style.border = "solid 10px red";
-        } else {
-          otherVideos[vl].elt.style.border = "";
-        }
+    for (const vl in otherVideos) {
+      if (loudest == vl) {
+        otherVideos[loudest].elt.style.border = 'solid 10px red';
+      } else {
+        otherVideos[vl].elt.style.border = '';
       }
+    }
   }
 }
 
@@ -69,7 +74,7 @@ function getAverageVolume(array) {
 
   // get all the frequency amplitudes
   for (var i = 0; i < length; i++) {
-      values += array[i];
+    values += array[i];
   }
 
   average = values / length;
@@ -82,16 +87,16 @@ function gotStream(stream, id) {
   otherVideo = stream;
   //otherVideo.id and id are the same and unique identifiers
   //otherVideo.hide();
-  
+
   var analyser = audioCtx.createAnalyser();
   console.log(stream);
   var source = audioCtx.createMediaStreamSource(stream.elt.srcObject);
   source.connect(analyser);
   stream.analyser = analyser;
-  
+
   otherVideos[id] = stream;
 }
 
 function gotDisconnect(id) {
-  delete otherVideos[id]; 
+  delete otherVideos[id];
 }
